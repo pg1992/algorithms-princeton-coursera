d5463cfa21498ab76f83112d9d6f53ad3620545e
Pedro Guilherme Siqueira Moreira <pedro.eletronica.unb@gmail.com>
2018-11-13 11:07:59 -0200

    Create a JUnit test script for TDD

---
+++ week04-8puzzle/test/SolverTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SolverTest {
+
+	@Test
+	public void testSolver() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsSolvable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMoves() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSolution() {
+		fail("Not yet implemented");
+	}
+
+}
