ef4fc0017a157c08993392b38bca10d0bdfa5e17
Pedro Guilherme Siqueira Moreira <pedro.eletronica.unb@gmail.com>
2018-11-16 17:58:37 -0200

    Working implementation of Solver with some tests

--- week04-8puzzle/src/Solver.java
+++ week04-8puzzle/src/Solver.java
@@ -1,4 +1,9 @@
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.LinkedList;
+
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.MinPQ;
 import edu.princeton.cs.algs4.StdOut;
 
 /**
@@ -7,50 +12,143 @@ import edu.princeton.cs.algs4.StdOut;
  *
  */
 public class Solver {
-	
+
+	private final Comparator<PQNode> PRIORITY_COMPARISON = new PriorityComparison();
+
+	private MinPQ<PQNode> mainMoves = new MinPQ<>(PRIORITY_COMPARISON);
+	private MinPQ<PQNode> twinMoves = new MinPQ<>(PRIORITY_COMPARISON);
+
+	private Deque<PQNode> steps = new LinkedList<>();
+
+	private boolean solvable;
+
 	/**
 	 * Find a solution to the {@code initial} board (using the A* algorithm).
 	 * 
 	 * @param initial a {@link Board} object with the initial configuration
 	 */
 	public Solver(Board initial) {
-		
+		PQNode cur;
+		PQNode curTwin;
+
+		mainMoves.insert(new PQNode(initial, null, 0));
+		twinMoves.insert(new PQNode(initial.twin(), null, 0));
+		steps.addFirst(new PQNode(initial, null, 0));
+
+		while (true) {
+			cur = mainMoves.delMin();
+			curTwin = twinMoves.delMin();
+
+			steps.addLast(cur);
+
+			if (cur.board.isGoal()) {
+				solvable = true;
+				break;
+			}
+
+			if (curTwin.board.isGoal()) {
+				solvable = false;
+				break;
+			}
+
+			for (Board neighbor : cur.board.neighbors()) {
+				if (!neighbor.equals(cur.prev))
+					mainMoves.insert(new PQNode(neighbor, cur.board, cur.moves + 1));
+			}
+
+			for (Board neighbor : curTwin.board.neighbors()) {
+				if (!neighbor.equals(curTwin.prev))
+					twinMoves.insert(new PQNode(neighbor, curTwin.board, curTwin.moves + 1));
+			}
+		}
+
+		Deque<PQNode> revert = new LinkedList<>();
+		revert.addLast(steps.removeLast());
+
+		while (!revert.peekFirst().board.equals(steps.peekFirst().board)) {
+			cur = steps.removeLast();
+			if (cur.board.equals(revert.peekFirst().prev))
+				revert.addFirst(cur);
+		}
+
+		steps = revert;
 	}
-	
+
 	public boolean isSolvable() {
-		throw new UnsupportedOperationException();
+		return solvable;
 	}
-	
+
 	public int moves() {
-		throw new UnsupportedOperationException();
+		return steps.peekLast().moves;
 	}
-	
+
 	public Iterable<Board> solution() {
-		throw new UnsupportedOperationException();
+		Deque<Board> boardSteps = new LinkedList<>();
+
+		for (PQNode pqn : steps)
+			boardSteps.addLast(pqn.board);
+
+		return boardSteps;
 	}
-	
+
 	public static void main(String[] args) {
+		// create initial board from file
+		In in = new In(args[0]);
+		int n = in.readInt();
+		int[][] blocks = new int[n][n];
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++)
+				blocks[i][j] = in.readInt();
+		Board initial = new Board(blocks);
 
-	    // create initial board from file
-	    In in = new In(args[0]);
-	    int n = in.readInt();
-	    int[][] blocks = new int[n][n];
-	    for (int i = 0; i < n; i++)
-	        for (int j = 0; j < n; j++)
-	            blocks[i][j] = in.readInt();
-	    Board initial = new Board(blocks);
+		// solve the puzzle
+		Solver solver = new Solver(initial);
 
-	    // solve the puzzle
-	    Solver solver = new Solver(initial);
-
-	    // print solution to standard output
-	    if (!solver.isSolvable())
-	        StdOut.println("No solution possible");
-	    else {
-	        StdOut.println("Minimum number of moves = " + solver.moves());
-	        for (Board board : solver.solution())
-	            StdOut.println(board);
-	    }
+		// print solution to standard output
+		if (!solver.isSolvable())
+			StdOut.println("No solution possible");
+		else {
+			StdOut.println("Minimum number of moves = " + solver.moves());
+			for (Board board : solver.solution())
+				StdOut.println(board);
+		}
 	}
-	
+
+	private class PQNode {
+		final Board board;
+		final Board prev;
+		final int moves;
+
+		PQNode(Board board, Board prev, int moves) {
+			this.board = board;
+			this.prev = prev;
+			this.moves = moves;
+		}
+
+		int priority() {
+			return board.manhattan() + moves;
+		}
+
+		@Override
+		public String toString() {
+			StringBuilder s = new StringBuilder();
+
+			s.append(String.format("priority  = %2d\n", priority()));
+			s.append(String.format("moves     = %2d\n", moves));
+			s.append(String.format("manhattan = %2d\n", board.manhattan()));
+			s.append(board);
+
+			return s.toString();
+		}
+	}
+
+	private class PriorityComparison implements Comparator<PQNode> {
+
+		@Override
+		public int compare(PQNode n1, PQNode n2) {
+			return n1.priority() - n2.priority();
+		}
+
+	}
+
 }
--- week04-8puzzle/test/SolverTest.java
+++ week04-8puzzle/test/SolverTest.java
@@ -1,5 +1,8 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
@@ -9,24 +12,63 @@ import org.junit.Test;
  */
 public class SolverTest {
 
-	@Test
-	public void testSolver() {
-		fail("Not yet implemented");
+	private static Solver solvable;
+	private static Solver unsolvable;
+	private static Solver anotherSolvable;
+	private static Solver anotherUnsolvable;
+
+	@BeforeClass
+	public static void initAllSolvers() {
+		solvable = new Solver(new Board(new int[][] { { 0, 1, 3 }, { 4, 2, 5 }, { 7, 8, 6 }, }));
+
+		unsolvable = new Solver(new Board(new int[][] { { 1, 2, 3 }, { 4, 6, 5 }, { 7, 8, 0 }, }));
+
+		anotherSolvable = new Solver(
+				new Board(new int[][] { { 1, 2, 4, 12 }, { 5, 6, 3, 0 }, { 9, 10, 8, 7 }, { 13, 14, 11, 15 }, }));
+
+		anotherUnsolvable = new Solver(
+				new Board(new int[][] { { 3, 2, 4, 8 }, { 1, 6, 0, 12 }, { 5, 10, 7, 11 }, { 9, 13, 14, 15 }, }));
 	}
 
 	@Test
-	public void testIsSolvable() {
-		fail("Not yet implemented");
+	public void testSolvable() {
+		assertTrue(solvable.isSolvable());
+	}
+
+	@Test
+	public void testUnsolvable() {
+		assertFalse(unsolvable.isSolvable());
+	}
+
+	@Test
+	public void testAnotherUnsolvable() {
+		assertFalse(anotherUnsolvable.isSolvable());
 	}
 
 	@Test
 	public void testMoves() {
-		fail("Not yet implemented");
+		assertEquals(4, solvable.moves());
 	}
 
 	@Test
-	public void testSolution() {
-		fail("Not yet implemented");
+	public void testSolutionLength() {
+		int solutionLength = 0;
+		for (Board sol : solvable.solution())
+			solutionLength++;
+		assertEquals(5, solutionLength);
+	}
+
+	@Test
+	public void testAnotherMoves() {
+		assertEquals(10, anotherSolvable.moves());
+	}
+
+	@Test
+	public void testAnotherSolutionLength() {
+		int solutionLength = 0;
+		for (Board sol : anotherSolvable.solution())
+			solutionLength++;
+		assertEquals(11, solutionLength);
 	}
 
 }
